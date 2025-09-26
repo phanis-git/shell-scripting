9049c6f623dc152de1f322bea5186594aa491758
PHANI <siva.palakeerthi@dru.care>
2025-09-26 22:32:34 +0530

    changes

--- variables05.sh
+++ variables05.sh
@@ -3,3 +3,9 @@
 todayDttm=$(date +%d-%m-%Y,%H:%M)
 
 echo "Present date and time: $todayDttm"
+
+startTime=$(date +%s)
+sleep 20
+endTime=$(date +%s)
+let totalTime=endTime-startTime
+echo "Total time :: $totalTime"
