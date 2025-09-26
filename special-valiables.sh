013a852a90c0af8c0071fd69b8734cdbf4283625
PHANI <siva.palakeerthi@dru.care>
2025-09-26 22:45:47 +0530

    changes

--- special-valiables.sh
+++ special-valiables.sh
@@ -4,5 +4,13 @@
 #In linux server while running script we need to pass values , as the * and @ will hold the values
 echo "These are special variables we can use * $*"
 echo "These are special variables we can use @ $@"
+
+# $0 will give the executing script name 
 echo "$0   this will give the executing script name"
+
+echo "current directory : $PWD"
+echo "current directory : $pwd"
+
+echo "current directory : $USER"
+echo "home directory : $HOME"
 
