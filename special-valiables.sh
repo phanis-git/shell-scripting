51e11a5a12d6091bf5d44347c45aa93a6f2e7b58
PHANI <siva.palakeerthi@dru.care>
2025-09-26 22:41:42 +0530

    changes

--- special-valiables.sh
+++ special-valiables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
-#In linux server while running script we need to pass values
+#In linux server while running script we need to pass values , as the * and @ will hold the values
 echo "These are special variables we can use * $*"
 echo "These are special variables we can use @ $@"
+echo "$0   this will give the executing script name"
+
