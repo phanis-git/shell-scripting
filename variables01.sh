f01e27b91d2a668c155de0c2777f35bb59dae698
PHANI <siva.palakeerthi@dru.care>
2025-09-26 19:34:06 +0530

    changes

---
+++ variables01.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1=Phani
+person2=Kiran
+
+# conversation
+
+echo "$person1:: Hi $person2"
+echo "$person2:: Hello $person1 , How are you"
+echo "$person1:: $person2 , I am good, what about you"
+echo "$person2:: I am also working fine $person1"
