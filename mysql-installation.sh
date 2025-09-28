cb30c200d85b52dbcb01d407fd4025388e72b247
PHANI <siva.palakeerthi@dru.care>
2025-09-28 19:19:14 +0530

    mysql-install-script

--- mysql-installation.sh
+++ mysql-installation.sh
@@ -1,12 +1,13 @@
 rootUserId=$(id -u)
 
-if [$rootUserId -eq 0] then;
+if [ $rootUserId -eq 0 ]; then
     dnf install mysql-server -y
     mysql_server_install_status=$?
-    if [$mysql_server_install_status -eq 0] then;
+    if [$mysql_server_install_status -eq 0]; then
     echo "Mysql server installed successfully"
     else 
     echo "There is a problem in Mysql server installation"
+    fi 
     else 
     echo "User doesnot have sudo or root access , Take root access "
 fi 
