977a944c2acf0b36029a486f33523923747740c7
PHANI <siva.palakeerthi@dru.care>
2025-09-29 13:13:50 +0530

    changes

--- logs.sh
+++ logs.sh
@@ -4,18 +4,24 @@ RED="\e[31m"
 GREEN="\e[32m"
 NORMAL_COLOR="\e[0m"
 isRootUser=$(id -u)
+
 if [ $isRootUser -eq 0 ]; then
     echo "Root user"
     else 
     echo -e "$RED Root user privelege is needed"
     exit 1
 fi
-
+validateInstalls(){
+    if [ $1 -eq 0 ]; then
+    echo -e "$2 is $GREEN installed successfully $NORMAL_COLOR"
+    else 
+    echo -e "$2 is $RED failed to install $NORMAL_COLOR"
+    fi 
+}
 # Installing mysql server and mysql client if not exist
 dnf list installed mysql-server
 if [ $? -eq 0 ]; then
     echo "Mysql-server already installed"
-    exit 1
     else 
     dnf install mysql-server -y
     validateInstalls $? "Mysql-server" 
@@ -25,19 +31,12 @@ fi
 dnf list installed mysql
 if [ $? -eq 0 ]; then
     echo "Mysql-client already installed"
-    exit 1
     else 
     dnf install mysql -y
     validateInstalls $? "Mysql-client" 
     exit 1
 fi
 
-validateInstalls(){
-    if [ $1 -eq 0 ]; then
-    echo "$2 is $GREEN installed successfully $NORMAL_COLOR"
-    else 
-    echo "$2 is $RED failed to install $NORMAL_COLOR"
-    fi 
-}
+
 
 
