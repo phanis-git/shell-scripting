9d4e713cdf37bdb1f8057037e946442d32db39b1
PHANI <siva.palakeerthi@dru.care>
2025-09-29 13:27:31 +0530

    changes

--- logs.sh
+++ logs.sh
@@ -4,7 +4,7 @@ RED="\e[31m"
 GREEN="\e[32m"
 NORMAL_COLOR="\e[0m"
 isRootUser=$(id -u)
-
+logFileName="/var/log/shell-script-logs/installation-logs.log"
 if [ $isRootUser -eq 0 ]; then
     echo "Root user"
     else 
@@ -13,7 +13,7 @@ if [ $isRootUser -eq 0 ]; then
 fi
 validateInstalls(){
     if [ $1 -eq 0 ]; then
-    echo -e "$2 is $GREEN installed successfully $NORMAL_COLOR"
+    echo -e "$2 is $GREEN installed successfully $NORMAL_COLOR" 
     else 
     echo -e "$2 is $RED failed to install $NORMAL_COLOR"
     fi 
@@ -21,7 +21,7 @@ validateInstalls(){
 # Installing mysql server and mysql client if not exist
 dnf list installed mysql-server
 if [ $? -eq 0 ]; then
-    echo -e "$GREEN Mysql-server already installed $NORMAL_COLOR"
+    echo -e "$GREEN Mysql-server already installed $NORMAL_COLOR" | &>>logFileName
     else 
     dnf install mysql-server -y
     validateInstalls $? "Mysql-server" 
@@ -29,7 +29,7 @@ fi
 
 dnf list installed mysql
 if [ $? -eq 0 ]; then
-    echo -e "$GREEN Mysql-client already installed $NORMAL_COLOR"
+    echo -e "$GREEN Mysql-client already installed $NORMAL_COLOR" | &>>logFileName
     else 
     dnf install mysql -y
     validateInstalls $? "Mysql-client" 
@@ -37,7 +37,7 @@ fi
 
 dnf list installed nginx
 if [ $? -eq 0 ]; then
-    echo -e "$GREEN Nginx already installed $NORMAL_COLOR"
+    echo -e "$GREEN Nginx already installed $NORMAL_COLOR" | &>>logFileName
     else 
     dnf install nginx -y
     validateInstalls $? "Nginx" 
