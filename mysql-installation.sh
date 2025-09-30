131a7b5f74b3037db79d68143f8fc5a5405db3bb
PHANI <siva.palakeerthi@dru.care>
2025-09-30 22:13:59 +0530

    changes

--- mysql-installation.sh
+++ mysql-installation.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 
-rootUserId=$(id -u)
 
-if [ $rootUserId -eq 0 ]; then
+# Before writing shell script first we need to note what are all the steps need to do
+
+
+# Steps to install mysql-server 
+# 1. Check the user has root access or not
+# 2. If root then procced for download mysql-server else warn to run like root
+# 3. Check mysql-server is already installed or not 
+# 4. If Already installed notify user already installed else 
+# 5. Install mysql-server 
+# 6. Check if mysql-server is properly installed or not 
+# 7. If properly installed then notify user with success else failure
+
+# getting user id
+rootUserId=$(id -u)
+# checking root user or not
+if [ $rootUserId -ne 0 ]; then
+    echo "Try with root user "
+    exit 1
+fi
+
+# checking if package already exist or not
+dnf list installed mysql-server
+if [ $? -ne 0 ]; then
     dnf install mysql-server -y
-    mysql_server_install_status=$?
-    if [ $mysql_server_install_status -eq 0 ]; then
-    echo "Mysql server installed successfully"
-    else 
-    echo "There is a problem in Mysql server installation"
-    fi 
-    else 
-    echo "User doesnot have sudo or root access , Take root access "
-fi 
+    if [ $? -ne 0 ]; then
+        echo "Mysql-server is failed install"
+        else
+        echo "Mysql-server is installed successfully"
+    else
+    echo "Mysql-server is already exist"
+    exit 1
+fi    
+
+
+
+
+
+
+
+
+
+
+
