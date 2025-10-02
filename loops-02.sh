3a59fdc940aafdbd32b143c6a7f9968a0648028a
PHANI <siva.palakeerthi@dru.care>
2025-10-02 21:46:07 +0530

    changes

--- loops-02.sh
+++ loops-02.sh
@@ -2,7 +2,18 @@
 
 allItems=("Banana" "Apple" "Grapes")
 
+# for loops or (for in loop)
 for item in "${allItems[@]}"
     do 
     echo "Items :: $item"
+    done
+
+
+    # while loop
+
+    num=1
+    while [$num -le 5 ];
+    do 
+    echo "Number:: $num"
+    $num=$(($num+1))
     done
