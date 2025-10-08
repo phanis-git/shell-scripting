e9e33628f3204dfeb99d473efec583932d22b0b2
PHANI <siva.palakeerthi@dru.care>
2025-10-08 16:38:21 +0530

    shell-roboshop

--- trap.sh
+++ trap.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+trap 'echo "There is an error in line number :: $LINENO,and the command is: $BASH_COMMAND"' ERR
 
 echo "Start"
 dsjif
