a9c8002419f2cdcbf40f480cad93f30167697e8f
PHANI <siva.palakeerthi@dru.care>
2025-10-10 21:00:41 +0530

    logs

---
+++ delete-old-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Deleting logs
+
+source_directory="/home/ec2-user/app-logs"
+
+LogFiles=$(find . -type f -size -2k -name "*.log")
+
+while IFS= read -r file
+do 
+    echo "Log files less than 2kb :: $file"
+done
